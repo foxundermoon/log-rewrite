3e6d530bebd3362ab864ad9558203ea46fd7fbc9
fox <fox@vvfox.com>
2017-04-18 13:41:57 +0800

    add android test project

---
+++ rewrite-test-android/src/main/java/com/getui/test/MainActivity.java
@@ -0,0 +1,31 @@
+package com.getui.test;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+/**
+ * Created by fox on 18/04/2017.
+ */
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    ViewGroup container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //to do something
+//            }
+//        });
+    }
+}
---
+++ rewrite-test-android/src/main/java/com/getui/test/MainApplication.java
@@ -0,0 +1,13 @@
+package com.getui.test;
+
+import android.app.Application;
+/**
+ * Created by fox on 18/04/2017.
+ */
+public class MainApplication   extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
